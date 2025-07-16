0047004d32ffa2e1d28cbc129118d0bae0c36fec
Sandeep-144 <149867097+Sandeep-144@users.noreply.github.com>
2025-07-16 18:53:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ st.markdown("""
 
     /* Main padding to prevent overlap */
     .main > div {
-        padding-top: 135px !important;
+        padding-top: 175px !important;
     }
 
     @media (max-width: 788px) {
